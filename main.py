25e20cc5aa905346786f54506c5dedc691ffadf6
Hwclub1105 <9504.liao@gmail.com>
2023-05-22 12:24:27 +0100

    d

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Use ', ' to separate
+import matplotlib.pyplot as pllt
 
 with open('QuestionsToRedoTests.txt', 'r') as file:
     info = []
@@ -9,4 +10,14 @@ with open('QuestionsToRedoTests.txt', 'r') as file:
         info.append(line.strip().split(', '))
     for i in info:
         print(f'{i[1]}: {i[5]}')
-        
+    
+    #pllt.barh(
+        #[info[i][1] for i in range(1,length)][::-1],
+        #[int(info[i][4]) for i in range(1,length)][::-1],
+        #color="orange",
+        #)
+#pllt.xticks([i for i in range(101) if i % 5 == 0])
+#pllt.colorbar(["purple" for i in range(1,length) if int(info[i][4]) == 100])
+#pllt.show(False)
+
+            
